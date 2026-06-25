5cc9abdee174ef803e66a649f5ccc44ac6dfcc3e
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-06-25 17:34:01 +0530

    ternary , ifelse3

---
+++ src/conditionalStatement/Ternary.java
@@ -0,0 +1,14 @@
+public class Ternary {
+
+    public static void main(String[] args) {
+
+        int a = 63;
+        int b = 36;
+
+        boolean x = (a < b) ? true : false;
+        int y = (a > b) ? a : b;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
