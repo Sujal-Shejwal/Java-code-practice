59958b21bdc6f405800cbade2236d39fef4f1f80
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-06-27 15:34:34 +0530

    change forloop

--- src/loops/ForLoop2.java
+++ src/loops/ForLoop2.java
@@ -1,11 +1,11 @@
 // print reverse of a number
 
 import java.util.Scanner;
-public class ForLoop3 {
+public class ForLoop2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int reverse = 20043;
+
         while (n > 0) {
             int lastDigit = n % 10;
             System.out.print(lastDigit + " ");
