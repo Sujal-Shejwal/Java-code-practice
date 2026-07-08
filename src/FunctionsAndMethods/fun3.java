8bd9330e1b454528834cfb42a1d1b731d277b571
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-07-08 16:09:19 +0530

    practiced functions

---
+++ src/FunctionsAndMethods/fun3.java
@@ -0,0 +1,15 @@
+// find product of a & b.
+import java.util.*;
+public class fun3 {
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int product = multiply(a, b);
+        System.out.println("a * b = " + product);
+    }
+}
