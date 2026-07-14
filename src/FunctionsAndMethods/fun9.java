2f5e694f46f440a8ae775fd153de180a9b769b2f
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-07-14 10:55:49 +0530

    practiced functions

--- src/FunctionsAndMethods/fun9.java
+++ src/FunctionsAndMethods/fun9.java
@@ -1,1 +1,30 @@
 // Print all primes in a range
+
+
+public class fun9 {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void primesInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        primesInRange(100);
+    }
+}
