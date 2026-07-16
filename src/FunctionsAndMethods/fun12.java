77f12d74be5df78cbb6d02cb76057bb00206a57e
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-07-16 19:45:21 +0530

    practiced functions and method

---
+++ src/FunctionsAndMethods/fun12.java
@@ -0,0 +1,30 @@
+// find binomial coefficient
+
+import java.util.Scanner;
+public class fun12 {
+    // Method to calculate factorial
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    // Method to calculate Binomial Coefficient
+    public static int binCoeff(int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n - r);
+        return fact_n / (fact_r * fact_nmr);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.print("Enter r: ");
+        int r = sc.nextInt();
+        // Correct method call
+        System.out.println("Binomial Coefficient = " + binCoeff(n, r));
+        sc.close();
+    }
+}
