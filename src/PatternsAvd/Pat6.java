9b6d556df33d1a42094911fae03cb1fbdd38b19c
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-07-22 19:42:54 +0530

    practiced advance patterns

--- src/PatternsAvd/Pat6.java
+++ src/PatternsAvd/Pat6.java
@@ -1,4 +1,41 @@
-package PatternsAvd;
+// Print butterfly Pattern.
+
+import java.util.Scanner;
 
 public class Pat6 {
+    public static void butterfly(int n) {
+        // first half
+        for (int i =1; i<=n; i++) {
+            for (int j =1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for (int j=1; j<=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Second half
+        for (int i =n; i>=1; i--) {
+            for (int j =1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for (int j=1; j<=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        butterfly(n);
+        sc.close();
+    }
 }
