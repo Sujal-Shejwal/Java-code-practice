d1a5dcd10680ea87bf9bc89afe0167ca5bcf7ba3
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-07-28 10:33:56 +0530

    practiced arrays

---
+++ src/array/arr6.java
@@ -0,0 +1,21 @@
+// print sum of subarrays.
+import java.util.*;
+public class arr6 {
+    public static void subarraysSum(int numbers[]){
+        for (int i=0; i< numbers.length; i++){
+            for (int j=i; j < numbers.length; j++){
+                int sum=0;
+                for (int k=i; k<=j; k++) {
+                    sum += numbers[k];
+
+                }
+                System.out.println("Sum = "+sum);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+        subarraysSum(numbers);
+    }
+}
